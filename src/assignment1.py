41773e43164c989bbe12cb45e8fe03775ef40837
Stephen Foster <stephenfostercpp@gmail.com>
2023-03-11 00:48:26 -0800

    fix for stdin from cmd and powershell

--- src/assignment1.py
+++ src/assignment1.py
@@ -11,24 +11,29 @@ from utils import stdin_has_input
 from utils import redirect_stdin_to_tempfile
 from utils import running_interactively
 
+import os
 import sys
 
 def main(argc, argv):
     if stdin_has_input():
         tmp = redirect_stdin_to_tempfile()
         db.batch_processor([tmp.name])
+        os.unlink(tmp.name)
 
     elif argc == 1 and running_interactively():
+        print("interpreter")
         db.interpreter()
 
     elif (argc == 1 and not running_interactively()) or (argc == 2 and argv[1].lower() =='gui'):
+        print("gui")
         db.gui()
 
     elif argc >= 2:
+        print("batch processor file")
         db.batch_processor(argv[1:])
 
     else:
         raise Exception("Call Error")
-
+    
 if __name__=="__main__":
     sys.exit(main(len(sys.argv), sys.argv))
--- src/utils.py
+++ src/utils.py
@@ -3,17 +3,29 @@ import psutil
 import select
 import sys
 import tempfile
+import multiprocessing
+
+def __peek_stdin():
+    return sys.stdin.read(0)
 
 def stdin_has_input() -> bool:
     if os.name == "nt":
-        import msvcrt
-        return msvcrt.kbhit()
+        t = multiprocessing.Process(target=__peek_stdin, args=())
+        t.start()
+        t.join(1.0)
+        
+        if t.is_alive():
+            t.terminate()
+            return False
+        else:
+            return True
 
     elif os.name == "posix":
         return select.select([sys.stdin,],[],[],0.0)[0]
 
 def redirect_stdin_to_tempfile() -> tempfile:
-    tmp = tempfile.NamedTemporaryFile()
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    tmp.close()
     with open(tmp.name, "w") as file:
         instructions = sys.stdin.buffer.read().decode("utf-8")
         file.write(instructions)
