3f99be847259dd605072058d4b61a4bf9239da57
Stephen Foster <stephenfostercpp@gmail.com>
2023-03-14 00:42:23 -0700

    bootstrap now creates env & installs runtime deps

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -9,14 +9,47 @@ def ensure_pip(where):
 
 def install_virtualenv(where):
     args = [sys.executable, "-m", "pip", "install", "virtualenv"]
-    venv = subprocess.run(args, cwd=where)
-    return venv.returncode
+    install_venv = subprocess.run(args, cwd=where)
+    return install_venv.returncode
+
+def create_virtualenv(where):
+    print(where)
+    args = [sys.executable, "-m", "virtualenv", "venv"]
+    act_venv = subprocess.run(args, cwd=where)
+    return act_venv.returncode
+
+def bootstrap_venv(where):
+    extension = ''
+
+    if os.name == "nt":
+        extension = ".bat"
+
+    activate_script = os.path.realpath(where + "/venv/Scripts/activate" + extension)
+    print(activate_script)
+    args = [activate_script]
+    act_venv = subprocess.run(args, cwd=where)
+
+    venv_python =  os.path.realpath(where + "/venv/Scripts/python")
+    requirements = os.path.realpath(where + "/requirements.txt")
+    args = [venv_python, "-m", "pip", "install", "-r", requirements]
+    install_deps = subprocess.run(args, cwd=where)
+
+    deactivate_script = os.path.realpath(where + "/venv/Scripts/deactivate" + extension)
+    args = [deactivate_script]
+    deact_venv = subprocess.run(args, cwd=where)
+
+    return act_venv + install_deps + deact_venv
 
 def main(argc, argv):
     root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     pip = ensure_pip(root)
-    venv = install_virtualenv(root)
-    return 0 if (pip + venv) == 0 else 1
+    install_venv = install_virtualenv(root)
+    create_venv = create_virtualenv(root)
+    bootstr_venv = bootstrap_venv(root)
+
+    code = pip + install_venv + create_venv + bootstr_venv
+    return code
 
 if __name__=="__main__":
     sys.exit(main(len(sys.argv), sys.argv))
