328e5acf86c0bf9c064217501fd7be788789fd69
Stephen Foster <stephenfostercpp@gmail.com>
2023-03-13 23:57:02 -0700

    bootstrap pip and virtualenv

---
+++ bootstrap.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import sys
+
+def ensure_pip(where):
+    args = [sys.executable, "-m", "ensurepip"]
+    pip = subprocess.run(args, cwd=where)
+    return pip.returncode
+
+def install_virtualenv(where):
+    args = [sys.executable, "-m", "pip", "install", "virtualenv"]
+    venv = subprocess.run(args, cwd=where)
+    return venv.returncode
+
+def main(argc, argv):
+    root = os.path.dirname(os.path.abspath(__file__))
+    pip = ensure_pip(root)
+    venv = install_virtualenv(root)
+    return 0 if (pip + venv) == 0 else 1
+
+if __name__=="__main__":
+    sys.exit(main(len(sys.argv), sys.argv))
--- install-requirements.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get update && sudo apt-get install python3.9
-python3.9 -m pip install -r requirements.txt
