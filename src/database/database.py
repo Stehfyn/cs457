f05d3a0144be1aa8e39126c5ce66ff61c1c5e912
Stephen Foster <stephenfostercpp@gmail.com>
2023-03-17 19:50:49 -0700

    api change

--- src/database/database.py
+++ src/database/database.py
@@ -103,14 +103,14 @@ class DatabaseGUI:
     
     def set_font(self):
         #font = os.path.realpath(os.path.dirname(os.path.abspath(__file__)) + f"/MyriadPro-Light.ttf")
-        with EmbeddedResource("MyriadPro-Light.ttf") as font:
+        with EmbeddedResource("MyriadPro-Light.ttf", delete_on_exit=False) as font:
             with dpg.font_registry():
                 default_font = dpg.add_font(font, 20)
             dpg.bind_font(default_font)
 
     def set_icon(self):
         #icon = os.path.realpath(os.path.dirname(os.path.abspath(__file__)) + f"/unr-256x256.ico")
-        with EmbeddedResource("UniversityLogo RGB_block_n_blue.ico") as icon:
+        with EmbeddedResource("UniversityLogo RGB_block_n_blue.ico", delete_on_exit=False) as icon:
             dpg.set_viewport_small_icon(icon)
             dpg.set_viewport_large_icon(icon)
     
